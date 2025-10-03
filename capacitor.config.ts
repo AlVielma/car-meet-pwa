3017911b10648ae0202baac96d09790228e008ab
AlVielma <vielma7220@gmail.com>
2025-10-02 19:56:51 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Car-meet pwa',
+  webDir: 'www'
+};
+
+export default config;
